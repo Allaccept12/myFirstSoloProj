047f3bc9a1572a4010798befde19c6ae633d82c1
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-02-12 11:42:35 +0900

    [FEAT] CommentService 비즈니스 로직 추가

--- src/main/java/solo/project1/service/CommentService.java
+++ src/main/java/solo/project1/service/CommentService.java
@@ -4,6 +4,9 @@ package solo.project1.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import solo.project1.domain.Account;
+import solo.project1.domain.Comment;
+import solo.project1.dto.CommentRequestDto;
 import solo.project1.repository.CommentRepository;
 import solo.project1.repository.PostRepository;
 
@@ -15,5 +18,34 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
 
-    public createCommeent
+    public Long createComment(CommentRequestDto commentRequestDto, Long postId) {
+        Account account = postRepository.findById(postId).orElseThrow(NullPointerException::new);
+        Comment comment = Comment.builder()
+                .content(commentRequestDto.getContent())
+                .post(account)
+                .build();
+        return commentRepository.save(comment).getId();
+    }
+
+    public void updateComment(CommentRequestDto commentRequestDto,Long commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(NullPointerException::new);
+        comment.updateComment(commentRequestDto);
+    }
+
+    public void deleteComment(Long commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(NullPointerException::new);
+        comment.deleteComment();
+        commentRepository.deleteById(commentId);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
